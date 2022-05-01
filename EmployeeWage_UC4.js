5057ff052f2667fbb54bd6e1b425025e29266087
Prateekpai <prateekvasanthpai@gmail.com>
2022-05-01 14:02:46 +0530

    Calculate the Monthly Wage for 20 Working Days

---
+++ EmployeeWage_UC4.js
@@ -0,0 +1,31 @@
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOUR=4;
+const FULL_TIME_HOUR=8;
+const WAGE_PER_HOUR=20;
+
+
+function getWorkingHours(empCheck){
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return    PART_TIME_HOUR;
+     
+         case IS_FULL_TIME:
+             return FULL_TIME_HOUR
+
+         default:
+             return 0;
+    }
+}
+let emphrs=0;
+const NUM_OF_WORKING_DAYS=20;
+
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+    emphrs+=getWorkingHours(empCheck);
+
+}
+let empWage=emphrs*WAGE_PER_HOUR;
+console.log("Total Hrs: "+emphrs+" - "+"EmpWage:"+ empWage);
